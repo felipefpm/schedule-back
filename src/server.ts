12b5706bb87433ae24a6b776e05484ea01fc5c06
Felipe Pinto <felipepinto.fpm@gmail.com>
2024-03-15 12:25:31 -0300

    Ajustments in host and port de api

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,17 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { appRoutes } from "./routes";
 
+const PORT = 3333;
+const HOST = '0.0.0.0';
+
 const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 3333,
+  host: HOST,
+  port: PORT,
 }).then(() => {
   console.log('Server is running')
 })
