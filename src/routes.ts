f884f64c5d892d214e6bb6a5998eb050fa5795e6
Felipe Pinto <felipepinto.fpm@gmail.com>
2024-04-24 18:12:00 -0300

    wip

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
+
 export async function appRoutes(app: FastifyInstance) {
 
   // Criar Habito
